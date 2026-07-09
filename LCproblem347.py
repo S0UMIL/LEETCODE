7a98cb03c77e779e6c06ed9c9671126979040e97
Soumil Tiwary <soumil.tiwary@gmail.com>
2026-07-09 12:45:41 +0530

    Optimize topKFrequent with bucket sort approach
    Refactor topKFrequent method to use bucket sort for frequency counting.

--- LCproblem347.py
+++ LCproblem347.py
@@ -1,14 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        new = defaultdict(int)
-
-        for num in nums:
-            new[num] += 1
-
-        heap = []
-        for num, count in new.items():
-            heapq.heappush(heap, (count, num))
-            if len(heap) > k:
-                heapq.heappop(heap)
-
-        return [num for count, num in heap]
+        freq={}#hashmap
+        for num in nums:#adding frequencies of number in the hashmap
+            if num in freq:
+                freq[num]+=1
+            else:
+                freq[num]=1
+        buckets=[[] for _ in range(len(nums)+1)]#creating buckets (bucket sort) ie list of lists
+        for num,count in freq.items():
+            buckets[count].append(num)
+        res=[]
+        for num, count in range(len(buckets)-1,0,-1):# going from high freq to low freq since we require highest freq either ways
+            for num in bucket[count]:#adding numbers into the res list until we have k
+                res.append(num)
+                if len(res)==k:
+                    return res
+                
+        
