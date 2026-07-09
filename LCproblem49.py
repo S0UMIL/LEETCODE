41a7e1bd1b40203052e9afe1d0f1403e16c79e5e
Soumil Tiwary <soumil.tiwary@gmail.com>
2026-07-09 13:48:18 +0530

    Improve comments in groupAnagrams function
    Refactor comments for clarity and improve code readability.

--- LCproblem49.py
+++ LCproblem49.py
@@ -1,10 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        new={}#creating a dictionary
+       new={}#creating a hashmap
         for i in strs:
-            s = "".join(sorted(i))#first sorting the elements why? take case of "act" and "cat" if we sort this we get 2-act we can note this in the dictionary as its key:value
-            if s not in new:
-                new[s] = []
-            new[s].append(i)#adding those sorted things into the dictionary
-            
-        return list(new.values())
+            s="".join(sorted(i))#suppose a word like eat, it is sorted into a,e,t= aet and this sorting converts it into list in this form - [a,e,t] to join the whole into one word we do "".join
+            if s not in new:#if a aet not in hashmap then
+                new[s]=[]#create a new key simple
+            new[s].append(i)#if key already exists add it in form of list for ex- aet(key)=[ate] now comes a word tae which is also an anagram of aet so it is just appended into the list and becomes [ate,tae]
+        return list(new.values())#we have to return an list as asked in the question so list(..)
