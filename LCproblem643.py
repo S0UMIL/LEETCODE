469682602e143d0cbefb25fd42fc0ccbf6e26752
Soumil Tiwary <soumil.tiwary@gmail.com>
2026-07-17 22:55:15 +0530

    Refactor findMaxAverage for clarity and efficiency

--- LCproblem643.py
+++ LCproblem643.py
@@ -1,15 +1,13 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        left = 0
-        window_sum = 0
-        max_sum = float('-inf')#need answer with error less than 10^-5 
-
+        left=0
+        value_sum=0
+        max_sum=float('-inf')
         for right in range(len(nums)):
-            window_sum += nums[right]#adding all the elements into window
-
-            if right - left + 1 == k:
-                max_sum = max(max_sum, window_sum)
-                window_sum -= nums[left]
-                left += 1
+            value_sum+=nums[right]
+            if right-left+1==k:
+                max_sum=max(max_sum , value_sum)
+                value_sum-=nums[left]
+                left+=1
         return max_sum/k
         
